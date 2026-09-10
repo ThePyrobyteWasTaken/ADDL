8a6c3a3fd38a7b2f984969a6bd19dc164ac5f0f7
ThePyrobyteWasTaken <mustafaarabaci023@gmail.com>
2026-09-10 12:14:38 +0300

    Update Roulette.js

--- js/pages/Roulette.js
+++ js/pages/Roulette.js
@@ -186,9 +186,9 @@ export default {
                 video: lvl.verification,
             }));
             const list = [];
-            if (this.useMainList) list.push(...fullListMapped.slice(0, 30));
+            if (this.useMainList) list.push(...fullListMapped.slice(0, 40));
             if (this.useExtendedList) {
-                list.push(...fullListMapped.slice(30, 60));
+                list.push(...fullListMapped.slice(40, 80));
             }
 
             // random 100 levels
